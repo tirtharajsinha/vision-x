662c79c9dc4bc75431673709a53bc410deeeeb55
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-01-29 19:37:13 +0530

    Update settings.py

--- vision/vision/settings.py
+++ vision/vision/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8f4ju0w7*+mw24xg(-3bdz3oa@ud6^9x7lsn#_1-w(zrn&7p&3'
+SECRET_KEY = 'get your key link in repo readme.md'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
